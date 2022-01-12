5e8d5c34b1ddb84d668517ee0c6505fd960ef6de
xfy196 <xfy196@outlook.com>
2022-01-12 13:09:56 +0800

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,4 +3,4 @@ const envMap = {
   "production": "release"
 }
 console.log(envMap[process.env.NODE_ENV])
-export const apiUrl = `https://service-cec0jxfp-1256637096.sh.apigw.tencentcs.com/${envMap[process.env.NODE_ENV]}/api`
+export const apiUrl = `https://api.chronus.xxycode.top/${envMap[process.env.NODE_ENV]}/api`
